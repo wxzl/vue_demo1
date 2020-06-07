f9536b35fa2c611a270cbaa67dbe203104837f10
zl <2547991359@qq.com>
2020-06-07 15:20:52 +0800

    permission init

---
+++ src/permission.js
@@ -0,0 +1,27 @@
+import router from './router'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
+NProgress.configure({ showSpinner: false })
+
+const whiteList = ['/login']
+
+router.beforeEach(async(to, from, next) => {
+  NProgress.start()
+  const hasToken = false
+
+  if (hasToken) {
+    console.log('123')
+  } else {
+    if (whiteList.indexOf(to.path) !== -1) {
+      next()
+    } else {
+      next(`/login?redirect=${to.path}`)
+      NProgress.done()
+    }
+  }
+})
+
+router.afterEach(() => {
+  NProgress.done()
+})
